db6bd5582bc5e22cfd7f6622a001d50f3803a66d
David Rodriguez <david@bsdadm.com>
2021-03-06 11:22:10 -0500

    add api calls needed for frontend to work

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,61 @@
-module.exports = function (app) {
-  app.get('/api/:action', (req, res) => {
-    let action = req.params.action;
-    if (action) {
-      switch (action) {
-      case 'workouts':
-        res.json({ message: 'All your workout data will show up here' });
-        break;
+const db = require("../models");
 
-      default:
-        res.json({ Error: 'This action does not exist in our api' });
-        break;
-      }
-    }
+module.exports = function (app) {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        dbWorkout.forEach((workout) => {
+          let total = 0;
+          workout.exercises.forEach((e) => {
+            total += e.duration;
+          });
+          workout.totalDuration = total;
+        });
+
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $inc: { totalDuration: req.body.duration },
+        $push: { exercises: req.body },
+      },
+      { new: true }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        console.log("ALL WORKOUTS");
+        console.log(dbWorkout);
+
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   });
 };
